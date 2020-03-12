c0d0d3df5841338156ba8f332a8205a01d3a9e69
kiprono <42340517+MeKCK@users.noreply.github.com>
2020-03-12 09:52:06 +0300

    working vuex

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,33 +1,53 @@
+
 import Vue from 'vue';
-import VueRouter from 'vue-router';
-
-import TopStories from '../views/TopStories.vue';
-import DevelopingStories from '../views/DevelopingStories.vue';
-import MyFavorites from '../views/MyFavorites.vue';
 import Router from 'vue-router';
+import store from '../store';
 
-Vue.use(VueRouter);
+import TopStories from '@/views/TopStories.vue';
+import DevelopingStories from '@/views/DevelopingStories.vue';
+import MyFavorites from '@/views/MyFavorites.vue';
 
-export default new Router({
+Vue.use(Router);
+
+class RouteMeta {
+  title: string;
+
+  constructor({title}: { title: string }) {
+    this.title = title;
+  }
+}
+
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'top-stories',
-      component: TopStories
+      component: TopStories,
+      meta: new RouteMeta({ title: 'Top Stories' })
     },
     {
       path: '/develop-stories',
       name: 'develop-stories',
-      component: DevelopingStories
+      component: DevelopingStories,
+      meta: new RouteMeta({ title: 'Developing Stories' })
     },
     {
       path: '/my-favorites',
       name: 'my-favorites',
-      component: MyFavorites
+      component: MyFavorites,
+      meta: new RouteMeta({ title: 'Favorites' })
     }
   ]
 });
 
+// This callback runs before every route change, including on initial load
+router.beforeEach((to, from, next) => {
 
+  const routeMeta = to.meta as RouteMeta;
+  store.dispatch('topToolbar/changeTitle', routeMeta.title);
+  next();
+});
+
+export default router;
---
+++ src/types.ts
@@ -0,0 +1,25 @@
+export interface NewsArticle {
+    id: number;
+    title: string;
+    content: string;
+    dateString: string;
+    baseImageName: string;
+    articleType: ArticleType;
+    isFavourite: boolean;
+  }
+  
+  export enum ArticleType {
+    TopStory = 'TOP_STORY',
+    DevelopingStories = 'DEVELOPING_STORIES'
+  }
+
+  
+// Store root state
+export interface RootState {
+    topToolbar: TopToolbarState;
+  }
+  
+  // Store modules state
+  export interface TopToolbarState {
+    title: string;
+  }
